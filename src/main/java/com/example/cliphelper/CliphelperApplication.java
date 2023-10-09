24b86440e517e45224c43499084bbe72855133f2
bflykky <kkyxw@soongsil.ac.kr>
2023-10-09 20:31:42 +0900

    init: 클립헬퍼 프로젝트 서버 초기 세팅

---
+++ src/main/java/com/example/cliphelper/CliphelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.cliphelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliphelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliphelperApplication.class, args);
+	}
+
+}
